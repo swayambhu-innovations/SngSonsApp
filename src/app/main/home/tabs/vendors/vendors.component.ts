b5d329dbef438d4d5ebf7a36de4fddc86d0e2934
atulsingh369 <businessatul7684@gmail.com>
2023-12-17 23:38:43 +0530

    Updated UI

--- src/app/main/home/tabs/vendors/vendors.component.ts
+++ src/app/main/home/tabs/vendors/vendors.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { AccordionTypeExpensesComponent } from 'src/app/main/components/accordion-type-expenses/accordion-type-expenses.component';
+import { BarGraphTableComponent } from 'src/app/main/components/bar-graph-table/bar-graph-table.component';
+import { DayWiseSummaryComponent } from 'src/app/main/components/day-wise-summary/day-wise-summary.component';
+import { VoucherComponent } from 'src/app/main/components/voucher/voucher.component';
 
 @Component({
   selector: 'app-vendors',
   templateUrl: './vendors.component.html',
   styleUrls: ['./vendors.component.scss'],
+  imports: [
+    DayWiseSummaryComponent,
+    VoucherComponent,
+    BarGraphTableComponent,
+    AccordionTypeExpensesComponent,
+    IonicModule,
+  ],
   standalone: true,
 })
 export class VendorsComponent implements OnInit {
