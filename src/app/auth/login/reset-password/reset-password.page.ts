dd6fa14b8a4deec691d9e650de6f9e3613afe01d
atulsingh369 <businessatul7684@gmail.com>
2023-12-18 00:32:17 +0530

    Updated UI

---
+++ src/app/auth/login/reset-password/reset-password.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.page.html',
+  styleUrls: ['./reset-password.page.scss'],
+})
+export class ResetPasswordPage implements OnInit {
+  resetForm = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+}
