860285e8be4980ccf9d924fb64eda2cc917cc74a
atulsingh369 <businessatul7684@gmail.com>
2024-01-02 13:58:21 +0530

    Updated UI

--- src/app/main/main-component-routing.module.ts
+++ src/app/main/main-component-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
-import { HomePage } from './home/home.page';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
