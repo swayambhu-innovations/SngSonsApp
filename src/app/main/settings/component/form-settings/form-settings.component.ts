494c41740e6be28df0291e9555ba77fbc9e6a12f
Raghav-686 <92645959+Raghav-686@users.noreply.github.com>
2024-01-04 12:06:00 +0530

    form settings changes

--- src/app/main/settings/component/form-settings/form-settings.component.ts
+++ src/app/main/settings/component/form-settings/form-settings.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { FormSettingService } from "./form-settings.service";
+import { Config } from "src/app/config";
+import { LoadingController, ModalController } from "@ionic/angular";
+import { NotificationService } from "src/app/utils/notification";
 
 @Component({
     selector:'app-form-setting',
@@ -15,33 +18,59 @@ export class FormSettingComponent implements OnInit{
         variableType: new FormControl('', [Validators.required]),     
         lowerLimit: new FormControl('', [Validators.required]),
         upperLimit: new FormControl('', [Validators.required]),
-        active: new FormControl(true, []),
+        active: new FormControl(true,[]),
         createdAt: new FormControl(new Date(), []),
         isZSD: new FormControl(false, []),
         id: new FormControl('')
     });
-    ngOnInit(): void {
-        this.presentingElement = document.querySelector('.ion-page');
-        this.getSettings('voucher-pending-form');
+    isModalOpen:boolean=false;
+    public config = Config;
+    public formVariable='';
+    public variableData: any = {
+        'post-dlv-pending-form': [],
+        'voucher-pending-form': []
     }
 
     constructor(
-        private formSettingService: FormSettingService
-    ) {
-
+        private formSettingService: FormSettingService,
+        private loadingController: LoadingController,
+        private notificationService: NotificationService,
+        private modalCtrl : ModalController
+    ) {}
+    
+    async ngOnInit() {
+        this.presentingElement = document.querySelector('.ion-page');
+        let loader = await this.loadingController.create({message:'Please wait...'});
+        loader.present();
+        await this.getSettings(this.config.formSettingVariable.PostDlvPendingForm);
+        await this.getSettings(this.config.formSettingVariable.VoucherPendingForm);
+        loader.dismiss();
     }
 
+    openModal(event:any , formId:string) {
+        event.stopPropagation();
+        this.isModalOpen=true;
+        this.formVariable=formId;
+    }
+
+    closeModal(){
+        this.isModalOpen=false;     
+        this.formSettings.reset();
+    }
+ 
     async getSettings(formId: string) {
         const data = await this.formSettingService.getSettings(formId);
         const variables = data.docs.map((item) => {
             return {...item.data(), id: item.id}
-        })
-        console.log(variables);
+        });
+        this.variableData[formId] = variables;
     }
 
-    submitForm(){
-        console.log(this.formSettings.value);
-        // this.formSettingService.addSettings('voucher-pending-form', this.formSettings.value);
+    async submitForm(event:any){         
+        await this.formSettingService.addSettings(this.formVariable, this.formSettings.value);
         this.formSettings.reset();
+        this.getSettings(this.formVariable);
+        this.notificationService.showSuccess(this.config.messages.addedSuccessfully);
+        this.modalCtrl.dismiss();
     }
 } 
