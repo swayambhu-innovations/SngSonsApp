75e6d360ce8db4302c7bdb78febce85b71dca67a
atulsingh369 <businessatul7684@gmail.com>
2024-01-12 22:59:00 +0530

    updated notification

--- src/app/main/notifications/notifications.page.ts
+++ src/app/main/notifications/notifications.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { SharedService } from 'src/app/shared/shared.service';
 
 @Component({
   selector: 'app-notifications',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsPage implements OnInit {
   public isNotifications: boolean = true;
-  constructor() {}
+
+  constructor(
+    private sharedService: SharedService,
+    private navCtrl: NavController
+  ) {}
 
   ngOnInit() {}
+
+  refresh() {
+    this.sharedService.refresh.next(true);
+  }
+
+  goBack() {
+    this.navCtrl.back();
+  }
 }
