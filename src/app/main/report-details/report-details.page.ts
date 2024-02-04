29407bd08d06e15ad77f92b1540f3ff09d3e6922
shivam <shivamtyagi102@gmail.com>
2024-02-04 17:15:21 +0530

    Vehicle wise, Area wise report

--- src/app/main/report-details/report-details.page.ts
+++ src/app/main/report-details/report-details.page.ts
@@ -18,7 +18,6 @@ export class ReportDetailsPage implements OnInit {
   reportData: any;
   openColVariables: boolean = false;
   tableColumns: any;
-  isTableReady:boolean =  true;
   activeColumnCount: number;
   minimumActiveCount: number = 2;
 
@@ -86,21 +85,17 @@ export class ReportDetailsPage implements OnInit {
     }
     const activeColumnVar = this.tableColumns.filter((item:any) => item.isActive);
     this.activeColumnCount = activeColumnVar.length;
-    this.isTableReady = false;
     this.tableData = {
       activeColumns: activeColumnVar,
       tableData: this.reportData
     };
-    setTimeout(() => {
-      this.isTableReady = true;
-    }, 10);
-    
   }
 
   getReport(report:string){
     if(report.toLowerCase() == "vendor wise expenses report"){
-      this.vendors = this.shipmentService.vendorsById;
+      
       this.shipmentService.getAllShipments().then((shipmentData) => {
+        this.vendors = this.shipmentService.vendorsById;
         this.shipments =  shipmentData.docs.map((shipment) => {
           shipment.data()['vendorData'].map((data:any) => {
             if(!data.vendor){
@@ -139,22 +134,26 @@ export class ReportDetailsPage implements OnInit {
           {
             text: 'S No',
             identifier : 'serialNo',
-            isActive: true
+            isActive: true,
+            width: 1
           },
           {
             text: 'W/S code',
             identifier : 'WSCode',
-            isActive: true
+            isActive: true,
+            width: 3
           },
           {
             text: 'W/S name',
             identifier : 'WSName',
-            isActive: true
+            isActive: true,
+            width: 5
           },
           {
             text: 'Shipment Count',
             identifier : 'shipmentCount',
-            isActive: true
+            isActive: true,
+            width: 3
           },
         ];
         this.activeColumnCount = this.tableColumns.length;
@@ -166,10 +165,155 @@ export class ReportDetailsPage implements OnInit {
         });
         this.reportData = Object.keys(this.vendors).map((key) => this.vendors[key]);
         this.onChangeColumn();
-        // const filtered = Object.keys(this.vendors).filter((item:any) => {
-        //   return this.vendors[item].shipments?.length > 1;
-        // });
-        // console.log(filtered);
+      });
+    }
+    else if(report.toLowerCase() == "vehicle wise expenses report"){
+      
+      this.shipmentService.getAllShipments().then((shipmentData) => {
+        const vehicleData:any = {};
+        this.shipments =  shipmentData.docs.map((shipment) => {
+          if(!vehicleData[shipment.data()['vehicle']]){
+            vehicleData[shipment.data()['vehicle']] = {
+              dataArray: [],
+              vehicle : {}
+            };
+          }
+          vehicleData[shipment.data()['vehicle']] = {...vehicleData[shipment.data()['vehicle']],...shipment.data()};
+          vehicleData[shipment.data()['vehicle']].dataArray.push({...shipment.data()});
+          return { ...shipment.data(), id: shipment.id };
+        });
+        this.tableColumns = [
+          {
+            text: 'S No',
+            identifier : 'serialNo',
+            isActive: true,
+            width: 1
+          },
+          {
+            text: 'Vehicle No',
+            identifier : 'vehicle',
+            isActive: true,
+            width: 4
+          },
+          {
+            text: 'Transporter Name',
+            identifier : 'TransporterName',
+            isActive: true,
+            width: 4
+          },
+          {
+            text: 'Order Count',
+            identifier : 'VendorCount',
+            isActive: true,
+            width: 3
+          },
+        ];
+        this.activeColumnCount = this.tableColumns.length;
+        let vendorCount = 0;
+        Object.keys(vehicleData).map((vehicle:any) => {
+          vendorCount++;
+          vehicleData[vehicle]['serialNo'] = vendorCount;
+          vehicleData[vehicle]['VendorCount'] = vehicleData[vehicle].dataArray?.length || 0;
+        });
+        this.reportData = Object.keys(vehicleData).map((key) => vehicleData[key]);
+        this.onChangeColumn();
+      });
+    }
+    else if(report.toLowerCase() == "area wise expenses report"){
+      this.shipmentService.getAllShipments().then((shipmentData) => {
+        this.vendors = this.shipmentService.vendorsById;
+        this.shipments =  shipmentData.docs.map((shipment) => {
+          shipment.data()['vendorData'].map((data:any) => {
+            if(!data.vendor){
+              return;
+            }
+            if(!(this.vendors[data.vendor]?.['shipments'])){
+              this.vendors[data.vendor]['shipments'] = [];
+              const vendorShipment = [];
+              vendorShipment.push(data);
+              const shipmentObject = {
+                fullShipment : shipment.data(),
+                vendorShipment : vendorShipment
+              }
+              this.vendors[data.vendor]['shipments'].push(shipmentObject);
+            }
+            else{
+              const shipmentIndex = this.vendors[data.vendor]['shipments'].length;
+              if(this.vendors[data.vendor]['shipments'][shipmentIndex - 1]){
+                this.vendors[data.vendor]['shipments'][shipmentIndex - 1]['vendorShipment'].push(data);
+              }
+              else{
+                const vendorShipment = [];
+                vendorShipment.push(data);
+                const shipmentObject = {
+                  fullShipment : shipment.data(),
+                  vendorShipment : vendorShipment
+                }
+                this.vendors[data.vendor]['shipments'].push(shipmentObject);
+              }
+            }
+           
+          });
+          return { ...shipment.data(), id: shipment.id };
+        });
+        this.tableColumns = [
+          {
+            text: 'S No',
+            identifier : 'serialNo',
+            isActive: true,
+            width: 1
+          },
+          {
+            text: 'W/S Town',
+            identifier : 'WSTown',
+            isActive: true,
+            width: 3
+          },
+          {
+            text: 'Vendors Count',
+            identifier : 'VendorsCount',
+            isActive: true,
+            width: 5
+          },
+          {
+            text: 'Shipment Count',
+            identifier : 'TotalShipmentCount',
+            isActive: true,
+            width: 3
+          },
+        ];
+        this.activeColumnCount = this.tableColumns.length;
+        let vendorCount = 0;
+        Object.keys(this.vendors).map((vendor:any) => {
+          vendorCount++;
+          this.vendors[vendor]['serialNo'] = vendorCount;
+          this.vendors[vendor]['shipmentCount'] = this.vendors[vendor].shipments?.length || 0;
+        });
+        const vendorFinalData = Object.keys(this.vendors).map((key) => this.vendors[key]);
+        const areas:any = {};
+        vendorFinalData.map((item:any) => {
+          if(!item["WSTown"]){
+            return;
+          }
+          if(!areas[item["WSTown"]]){
+            areas[item["WSTown"]] = {};
+            areas[item["WSTown"]]['vendors'] = [];           
+          }
+          areas[item["WSTown"]]['vendors'].push(item);
+          areas[item["WSTown"]]['WSTown'] = item["WSTown"];
+        });
+        const areaFinalData = Object.keys(areas).map((key) => areas[key]);
+        areaFinalData.map((item:any,index) => {
+          item['VendorsCount'] = item.vendors?.length || 0;
+          let shipmentCount = 0;
+          item.vendors?.map((vendor:any) => {
+            shipmentCount+= (+(vendor.shipmentCount));
+          });
+          item['TotalShipmentCount'] = shipmentCount;
+          item['serialNo'] = index+1;
+        });
+        this.reportData = areaFinalData;
+        this.onChangeColumn();
       });
     }
   }
