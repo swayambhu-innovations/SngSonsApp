73262e09f6ae2158a9ae31267e033d20f03c3722
Atul Kumar Singh <atulsingh.0369@gmail.com>
2024-03-19 01:06:56 +0530

    got correct shipments

--- src/app/main/home/tabs/shipments/shipments.component.ts
+++ src/app/main/home/tabs/shipments/shipments.component.ts
@@ -17,43 +17,57 @@ export class ShipmentsComponent implements OnInit {
   isError: boolean = false;
   isData: boolean = false;
   loader: any;
-  
+
   tableData = [
     { name: 'Shipment ID', key: 'ShipmentNumber', size: '4' },
     { name: 'Party Name', key: 'CustomerName', size: '3' },
-    { name: 'Area', key: 'WSTown', size: '3' }
+    { name: 'Area', key: 'WSTown', size: '3' },
   ];
 
-  constructor (
+  constructor(
     public excelUploadService: ExcelUploadService,
     private importExportService: ImportExportService,
     private notification: NotificationService,
     private loadingController: LoadingController,
     public homeService: HomeService,
     private shipmentService: ShipmentsService
-  ) { }
+  ) {}
 
   async ngOnInit() {
-    this.loader = await this.loadingController.create({ message: Config.messages.pleaseWait })
-    await this.init()
+    this.loader = await this.loadingController.create({
+      message: Config.messages.pleaseWait,
+    });
+    await this.init();
   }
 
   async init() {
     this.loader.present();
-    (await this.importExportService.getAllVehicles()).docs.map((vehicle: any) => {
-      this.shipmentService.vehicles[vehicle.id] = { ...vehicle.data(), id: vehicle.id };
-      return { ...vehicle.data(), id: vehicle.id }
-    });
+    (await this.importExportService.getAllVehicles()).docs.map(
+      (vehicle: any) => {
+        this.shipmentService.vehicles[vehicle.id] = {
+          ...vehicle.data(),
+          id: vehicle.id,
+        };
+        return { ...vehicle.data(), id: vehicle.id };
+      }
+    );
     (await this.importExportService.getAllVendors()).docs.map((vendor: any) => {
-      this.shipmentService.vendors[vendor.data().WSName.split(' ').join('-').toLowerCase()] = { ...vendor.data(), id: vendor.id };
-      return { ...vendor.data(), id: vendor.id }
+      this.shipmentService.vendors[
+        vendor.data().WSName.split(' ').join('-').toLowerCase()
+      ] = { ...vendor.data(), id: vendor.id };
+      return { ...vendor.data(), id: vendor.id };
     });
     this.loader.dismiss();
   }
 
   addZSD(event: any, data: any, formatDate: any, scope: any) {
     data = scope.importExportService.formatShipment(data, formatDate);
-    scope.importExportService.addShipments(data, scope, scope.loader, scope.notification);
-    event.target.value = "";
+    scope.importExportService.addShipments(
+      data,
+      scope,
+      scope.loader,
+      scope.notification
+    );
+    event.target.value = '';
   }
 }
