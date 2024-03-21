8d05129c4bfcbaff6f7dbdfbc7c55c90ef8217da
Atul Kumar Singh <atulsingh.0369@gmail.com>
2024-03-21 11:11:52 +0530

    other bugs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,14 +12,7 @@ export const environment = {
     messagingSenderId: '655441091349',
     measurementId: 'G-64R21R86LX',
   },
-  // cloudFunctions : {
-  //   createOrder: 'https://us-central1-jaipurservicecompany.cloudfunctions.net/createOrder',
-  //   capturePayment: 'https://us-central1-jaipurservicecompany.cloudfunctions.net/capturePayments',
-  //   createSubscription: 'https://us-central1-jaipurservicecompany.cloudfunctions.net/createSubscription',
-  //   verifySubscription:'https://us-central1-jaipurservicecompany.cloudfunctions.net/verifySubscription',
-  //   checkSubscriptionStatus:'https://us-central1-jaipurservicecompany.cloudfunctions.net/checkSubscriptionStatus',
-  // },
-  // RAZORPAY_KEY_ID: 'rzp_test_8cTBlk022y2EDq',
+  
   production: false
 };
 
